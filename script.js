2c183627e149a2e9bb880bdee98427be9a4e51dd
repl1994 <pgldaly@gmail.com>
2021-01-07 05:58:28 +0000

    updated header added auto slideshow css for slideshow commented out showcase img

--- script.js
+++ script.js
@@ -1,20 +1,53 @@
 
 
 $(document).ready(function(){
+  // BODY
+  $("body").css("font-family","sans-serif");
+
   // HEADER
-  var h1 = $("<h1></h1>").text("Software Developer");
-  var h6 = $("<h6></h6>").text("pgldaly@gmail.com");
-  $("body").append(h1, h6);
-  $(":header").css("background-color","green");
-  $(":header").css("color","white");
+  var div_header = $("<div id='div_header'></div>");
+  var softDev = $("<h1></h1>").text("Software Developer");
+  var name = $("<h3></h3>").text("Patrick Daly");
+  var email = $("<h6></h6>").text("pgldaly@gmail.com");
+  $("body").append(div_header);
+  $("#div_header").append(softDev,name,email);
+  $(":header").css("margin","0");
+  $("#div_header").css("background-color","green");
+  $("#div_header").css("color","white");
+  
+  // SLIDESHOW
+  var slideshow = $("<div id='slideshow'></div>");
+  var slide_0 = $("<div id='slide_0' class='slides'></div>");
+  var img_0 = $("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg'>");
+  var slide_1 = $("<div id='slide_0' class='slides'></div>");
+  var img_1 = $("<img src='https://wallpaperaccess.com/full/36296.jpg'>");
+  $("body").append(slideshow);
+  $("#slideshow").append(slide_0);
+  $("#slideshow").append(slide_1);
+  $("#slide_0").append(img_0);
+  $("#slide_0").append(img_1);
+  //$("#slideshow").css("")
+  $("#slideshow > div:gt(0)").hide();
+
+  setInterval(function() { 
+  $('#slideshow > div:first')
+    .fadeOut(1000)
+    .next()
+    .fadeIn(1000)
+    .end()
+    .appendTo('#slideshow');
+  },  3000);
+
 
   // INTRO
-  var p = $("<p></p>").text("Hello, my name is Patrick Glen Louis Daly. I am a software developer.")
+  var p = $("<p></p>").text("This webpage uses _____")
   $("body").append(p);
   $("p").css("background-color","green");
   $("p").css("color","white");
 
+/*
   // SHOWCASE
   var img = $("<img src='https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/Shaqi_jrvej.jpg/1200px-Shaqi_jrvej.jpg'>");
   $("body").append(img);
+  */
 });
